9e35dc818973ec783711e3515ac7575183fdb58d
akshay <akshaykrishnam80@gmail.com>
2024-01-28 18:25:44 +0000

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const logger = require('morgan');
+
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+
+
+
+app.use(cors());
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("App started on port: " + port);
+});
