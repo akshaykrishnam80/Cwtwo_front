864559bbd82e7fd05c3039dfaf31967c211ec229
akshay <akshaykrishnam80@gmail.com>
2024-01-28 21:05:47 +0000

    added url, put method declare, post in order done

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { client,dbName, connectToMongo } = require('./config');
 const cors = require('cors');
 
 const loger = require('morgan');
+const bodyParser = require('body-parser');
 
 
 const fs = require('fs');
@@ -16,6 +17,9 @@ app.use(loger('dev'));
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log', 'access.log'), { flags: 'a' });
 
+app.use(bodyParser.json()); // for parsing application/json
+
+
 app.use(loger('combined', { stream: accessLogStream }));
 
 
@@ -23,14 +27,14 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use(async (req, res, next) => {
-    req.dbClient = client;
+app.use(async (request, res, next) => {
+    request.dbClient = client;
     next();
 });
 
-app.get('/collection/:param', async (req, res) => {
-    const { param } = req.params;
-    const collection = req.dbClient.db(dbName).collection(param);
+app.get('/collection/:param', async (request, res) => {
+    const { param } = request.params;
+    const collection = request.dbClient.db(dbName).collection(param);
 
     try {
         const data = await collection.find().toArray();
@@ -46,13 +50,23 @@ app.post('/order', async (request, response) => {
         const order = request.body;
         console.log('Received order:', order);
 
-        // Process the order here (e.g., save to database)
+        const collection = request.dbClient.db(dbName).collection('orders');
+
+        await collection.insertOne(order);
 
         response.status(201).send({ message: 'Order received successfully', order });
     } catch (error) {
         console.error('Error processing order:', error);
         response.status(500).send({ error: 'Error processing order' });
     }
+});
+
+app.put('/lesson/:id', async(request, response)=>{
+const {id} = request.params;
+const {quantity} = request.body;
+
+console.log(quantity);
+
 });
 
 app.listen(port, () => {
